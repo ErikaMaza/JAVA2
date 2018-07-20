963294a4a3a0d7559cdd10443994ef164a7702c1
ErikaMaza <erika.maza@mail.ru>
2018-07-20 21:45:11 +0300

    дополнение

---
+++ dz_lesson4_fx/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+
+public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    Button button;
+
+}
